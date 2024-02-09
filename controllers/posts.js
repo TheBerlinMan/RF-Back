f8a461c9338991714f88c6bb560ddbe0ca5cd719
Berlin <tommyonik@gmail.com>
2024-02-08 20:18:07 -0500

    build createPost controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,20 @@ import { Post } from "../models/post.js"
 
 
 async function create(req, res){
-
+  try {
+    req.body.carPal = req.user.profile
+    const post = await Post.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { posts: post } },
+      { new: true }
+    )
+    post.carPal = profile
+    res.json(post)
+  } catch (error) {
+    console.log(error)
+    res.json(error)
+  }
 }
 
 export {
